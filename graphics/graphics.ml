a66f9789fd959bfeeb1c66a57d0044dd14a21446
Lauren Lee <lsl88@cornell.edu>
2023-03-22 02:29:00 -0400

    Comment clarification
    -Added comment in graphics.ml clarifying where online resources were used

--- graphics/graphics.ml
+++ graphics/graphics.ml
@@ -1,32 +1,34 @@
 open Raylib
+
+(*setup and loop based off of example code provided from RayLib GitHub
+  https://github.com/tjammer/raylib-ocaml.git*)
+
 let setup () =
   init_window 1000 900 "Minesweeper";
   set_mouse_scale 1. 1.
+
 let rec loop () =
-    if Raylib.window_should_close () then Raylib.close_window ()
-    else
-      let open Raylib in
-      begin_drawing ();
-      clear_background Color.raywhite;
-      draw_text "minesweeper? i hardly know her!" 350 450 20 Color.black;
-      end_drawing ();
-      loop ()
+  if Raylib.window_should_close () then Raylib.close_window ()
+  else
+    let open Raylib in
+    begin_drawing ();
+    clear_background Color.raywhite;
+    draw_text "minesweeper? i hardly know her!" 350 450 20 Color.black;
+    end_drawing ();
+    loop ()
+
 let () = setup () |> loop
-let boxWidth:int = 20
-let xCoord= ref 0
-let yCoord= ref 0
-let rec drawGrid m =  
-  for i=0 to Array.length m-1 do 
-    for j=0 to Array.length (Array.get m i) do 
-      if (Array.get (Array.get m i) j) = -1 then draw_rectangle !xCoord !yCoord boxWidth boxWidth Color.red 
+let boxWidth : int = 20
+let xCoord = ref 0
+let yCoord = ref 0
+
+let rec drawGrid m =
+  for i = 0 to Array.length m - 1 do
+    for j = 0 to Array.length (Array.get m i) do
+      if Array.get (Array.get m i) j = -1 then
+        draw_rectangle !xCoord !yCoord boxWidth boxWidth Color.red
       else draw_rectangle !xCoord !yCoord boxWidth boxWidth Color.gray;
-      yCoord:= !yCoord + boxWidth
+      yCoord := !yCoord + boxWidth
     done;
-  xCoord:= !xCoord + boxWidth
-done;
-
-
-
-
-
-
+    xCoord := !xCoord + boxWidth
+  done
