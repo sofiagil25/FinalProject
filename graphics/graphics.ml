d39d213e916f634a555e46139e446b1d56a26ffa
Lauren Lee <lsl88@cornell.edu>
2023-03-22 21:12:16 -0400

    Fixed IndexOutOfBounds error
    -In graphics.ml, line 10

--- graphics/graphics.ml
+++ graphics/graphics.ml
@@ -7,7 +7,7 @@ let yCoord = ref 0
 
 let rec drawGrid m =
   for i = 0 to Array.length m - 1 do
-    for j = 0 to Array.length (Array.get m i) do
+    for j = 0 to Array.length (Array.get m i) - 1 do
       if Board.ismine m i j = -1 then
         draw_rectangle !xCoord !yCoord boxWidth boxWidth Color.red
       else draw_rectangle !xCoord !yCoord boxWidth boxWidth Color.gray;
@@ -18,8 +18,7 @@ let rec drawGrid m =
 
 let setup () =
   init_window 1000 900 "Minesweeper";
-  set_mouse_scale 1. 1.;
-  drawGrid (Board.newboard 10 10)
+  set_mouse_scale 1. 1.
 
 let rec loop () =
   if Raylib.window_should_close () then Raylib.close_window ()
@@ -28,6 +27,7 @@ let rec loop () =
     begin_drawing ();
     clear_background Color.raywhite;
     draw_text "minesweeper? i hardly know her!" 350 450 20 Color.black;
+    drawGrid (Board.newboard 10 10);
     end_drawing ();
     loop ()
 
