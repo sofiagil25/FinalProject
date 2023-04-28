13fda672a997145a1239d3104be08304d68f1de8
Derek <dnz4@cornell.edu>
2023-04-27 20:54:23 -0400

    Implement clickability on all tiles
    - Upon clicking a tile, it turns green on the board and stays green
    - Created tile graphic storage system that tracks clicks on tiles

--- graphics/graphics.ml
+++ graphics/graphics.ml
@@ -3,10 +3,43 @@ open Game
 
 let boxWidth : int = 80
 
+let setup () =
+  init_window 1000 900 "Minesweeper";
+  set_mouse_scale 1. 1.
+
+let thisgameboard = Board.newboard 10 12 40
+
+let rec buildRects m =
+  let rects =
+    Array.make_matrix (Array.length m)
+      (Array.length m.(0))
+      (Rectangle.create 0. 0. 0. 0., false)
+  in
+  for i = 0 to Array.length m - 1 do
+    for j = 0 to Array.length (Array.get m i) - 1 do
+      let rect =
+        Rectangle.create
+          (i * boxWidth |> float_of_int)
+          (j * boxWidth |> float_of_int)
+          (boxWidth |> float_of_int) (boxWidth |> float_of_int)
+      in
+      rects.(i).(j) <- (rect, false)
+    done
+  done;
+  rects
+
+let rects = buildRects thisgameboard
+
 let rec drawGrid m =
   for i = 0 to Array.length m - 1 do
     for j = 0 to Array.length (Array.get m i) - 1 do
-      if Board.ismine m i j = -1 then
+      if
+        match rects.(i).(j) with
+        | _, bool -> bool
+      then
+        draw_rectangle (i * boxWidth) (j * boxWidth) boxWidth boxWidth
+          Color.green
+      else if Board.ismine m i j = -1 then
         draw_rectangle (i * boxWidth) (j * boxWidth) boxWidth boxWidth Color.red
       else (
         draw_rectangle (i * boxWidth) (j * boxWidth) boxWidth boxWidth
@@ -19,11 +52,21 @@ let rec drawGrid m =
     done
   done
 
-let setup () =
-  init_window 1000 900 "Minesweeper";
-  set_mouse_scale 1. 1.
-
-let thisgameboard = Board.newboard 10 12 40
+let findCollision mouse_pos =
+  let rows = Array.length rects in
+  let cols = Array.length rects.(0) in
+  let final_rect = ref None in
+  for i = 0 to rows - 1 do
+    for j = 0 to cols - 1 do
+      let rectangle =
+        match rects.(i).(j) with
+        | rectangle, _ -> rectangle
+      in
+      if check_collision_point_rec mouse_pos rectangle then
+        rects.(i).(j) <- (rectangle, true);
+      final_rect := Some (rectangle, true)
+    done
+  done
 
 let rec loop () =
   if Raylib.window_should_close () then Raylib.close_window ()
@@ -34,7 +77,10 @@ let rec loop () =
     draw_text "minesweeper? i hardly know her!" 350 450 20 Color.black;
     if is_cursor_on_screen () then (
       clear_background Color.raywhite;
-      drawGrid (Board.boardwithvalue thisgameboard));
+      drawGrid (Board.boardwithvalue thisgameboard);
+      if is_mouse_button_pressed MouseButton.Left then
+        let mouse_pos = get_mouse_position in
+        findCollision (mouse_pos ()));
     end_drawing ();
     loop ()
 
